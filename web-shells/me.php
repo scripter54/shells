4e7d1cceedf5942d37b13d58a14725352b241a10
scripter <83101169+scripter54@users.noreply.github.com>
2023-02-23 20:03:40 +0300

    Add files via upload

---
+++ web-shells/me.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>| scripter</title>
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+	<style type="text/css">
+		body{
+	background-color: #444;
+	background: url("https://getwallpapers.com/wallpaper/full/4/3/d/1063562-vertical-google-data-center-wallpaper-1920x1080-windows-7.jpg");
+	  content: "";
+	  position: fixed;
+	  top: 0; bottom: 0; left: 0; right: 0; 
+	  background: rgba(4,4,4,0.3);
+	
+}
+p{
+    font-size: 100px;
+    font-family: 'Courier New', monospace;
+    position: absolute;
+   	top: 25%;
+    left: 50%;
+    -moz-transform: translateX(-50%) translateY(-50%);
+    -webkit-transform: translateX(-50%) translateY(-50%);
+}
+h4{
+    font-size: 20px;
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    -moz-transform: translateX(-50%) translateY(-50%);
+    -webkit-transform: translateX(-50%) translateY(-50%);
+}
+.center{
+	  position: absolute;
+    top: 55%;
+    left: 50%;
+    -moz-transform: translateX(-50%) translateY(-50%);
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    width: 100%;
+    height: 100%;
+}
+a{
+	text-decoration: none;
+	color: black;
+}
+	</style>
+<div class="center">
+	<p>scripter</p>
+	<h4>penetration tester <a href="login.php">&</a> software engineer</h4>
+</div>
+</body>
+</html>
